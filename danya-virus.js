8c928367ae233cfae9b207b83fb83410c1e73f80
Yuii-Antoniuk <u983405@gmail.com>
2026-07-18 00:01:36 +0300

    done

---
+++ danya-virus.js
@@ -0,0 +1,28 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  if (!refs.openModalBtn || !refs.modal) {
+    return;
+  }
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+
+  if (refs.closeModalBtn) {
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  }
+
+  refs.modal.addEventListener("click", event => {
+    if (event.target === refs.modal) {
+      toggleModal();
+    }
+  });
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+    document.body.classList.toggle("no-scroll");
+  }
+})();
